68fc8117409662e84bffbb0912661bab56ff0e1d
bird <fgxb1rd@gmail.com>
2024-05-09 17:56:53 +0800

    fix: fix character movement issues

--- src/main.js
+++ src/main.js
@@ -1,59 +1,73 @@
 import kaboom from "kaboom";
 
-const k = kaboom();
+const k = kaboom({ font: "monospace" });
 
 const SPEED = 160;
 
 k.loadSprite("lucas", "sprites/lucas.png", {
-	sliceX: 4,
-    sliceY: 4,
-	anims: {
-		idleDown: {from: 0, to: 0},
-		idleLeft: {from: 4, to: 4},
-		idleRight: {from: 8, to: 8},
-		idleUp: {from: 12, to: 12},
-		moveDown: {from: 0, to: 3}, //, loop: true, speed: 1},
-		moveLeft: {from: 4, to: 7}, //, loop: true, speed: 1},
-		moveRight: {from: 8, to: 11}, //, loop: true, speed: 1},
-		moveUp: {from: 12, to: 15}, //, loop: true, speed: 1}
-	}
+  sliceX: 4,
+  sliceY: 4,
+  anims: {
+    idleDown: 0,
+    idleLeft: 4,
+    idleRight: 8,
+    idleUp: 12,
+    moveDown: { from: 0, to: 3, speed: 5, loop: true },
+    moveLeft: { from: 4, to: 7, speed: 5, loop: true },
+    moveRight: { from: 8, to: 11, speed: 5, loop: true },
+    moveUp: { from: 12, to: 15, speed: 5, loop: true },
+  },
 });
 
-const player = k.add([
-	k.pos(center()),
-	k.sprite("lucas"),
-])
+const player = k.add([k.pos(center()), k.sprite("lucas")]);
 
-// onKeyDown() registers an event that runs every frame as long as user is holding a certain key
-k.onKeyDown("left", () => {
-	// .move() is provided by pos() component, move by pixels per second
-	player.move(-SPEED, 0);
-	player.play("moveLeft");
-})
-// k.onKeyRelease("left", () => {
-// 	player.play("idleLeft");
-// })
+k.onUpdate(() => {
+  if (isKeyDown("left")) {
+    player.move(-SPEED, 0);
+    if (player.curAnim() !== "moveLeft") {
+      player.play("moveLeft");
+    }
+  }
+  if (isKeyReleased("left") && !isKeyDown("left")) {
+    player.play("idleLeft");
+  }
 
-k.onKeyDown("right", () => {
-	player.move(SPEED, 0);
-	player.play("moveRight");
-})
-// k.onKeyRelease("right", () => {
-// 	player.play("idleRight");
-// })
+  if (isKeyDown("right")) {
+    player.move(SPEED, 0);
+    if (player.curAnim() !== "moveRight") {
+      player.play("moveRight");
+    }
+  }
+  if (isKeyReleased("right") && !isKeyDown("right")) {
+    player.play("idleRight");
+  }
 
-k.onKeyDown("up", () => {
-	player.move(0, -SPEED);
-	player.play("moveUp");
-})
-// k.onKeyRelease("up", () => {
-// 	player.play("idleUp");
-// })
+  if (isKeyDown("up")) {
+    player.move(0, -SPEED);
+    if (player.curAnim() !== "moveUp") {
+      player.play("moveUp");
+    }
+  }
+  if (isKeyReleased("up") && !isKeyDown("up")) {
+    player.play("idleUp");
+  }
 
-k.onKeyDown("down", () => {
-	player.move(0, SPEED);
-	player.play("moveDown");
-})
-// k.onKeyRelease("down", () => {
-// 	player.play("idleDown");
-// })
+  if (isKeyDown("down")) {
+    player.move(0, SPEED);
+    if (player.curAnim() !== "moveDown") {
+      player.play("moveDown");
+    }
+  }
+  if (isKeyReleased("down") && !isKeyDown("down")) {
+    player.play("idleDown");
+  }
+});
+
+const getInfo = () =>
+  `Anim:  ${player.curAnim()}\nFrame: ${player.frame}\n`.trim();
+
+const label = k.add([text(getInfo, { size: 12 }), color(0, 0, 0), pos(4)]);
+
+label.onUpdate(() => {
+  label.text = getInfo();
+});
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,50 @@
+const directions = {
+  left: {
+    speed: [-SPEED, 0],
+    moveAnim: "moveLeft",
+    idleAnim: "idleLeft",
+  },
+  right: {
+    speed: [-SPEED, 0],
+    moveAnim: "moveRight",
+    idleAnim: "idleRight",
+  },
+  up: {
+    speed: [-SPEED, 0],
+    moveAnim: "moveUp",
+    idleAnim: "idleUp",
+  },
+  down: {
+    speed: [-SPEED, 0],
+    moveAnim: "moveDown",
+    idleAnim: "idleDown",
+  },
+};
+
 export function isOneMoveKeyDown(p) {
-    let isOnlyOneKeyDown = false;
-    for (const key of [p.RIGHT_ARROW, p.LEFT_ARROW, p.UP_ARROW, p.DOWN_ARROW]) {
-      if (!isOnlyOneKeyDown && p.keyIsDown(key)) {
-        isOnlyOneKeyDown = true;
-        continue;
-      }
-  
-      if (isOnlyOneKeyDown && p.keyIsDown(key)) {
-        return false;
-      }
+  let isOnlyOneKeyDown = false;
+  for (const key of [p.RIGHT_ARROW, p.LEFT_ARROW, p.UP_ARROW, p.DOWN_ARROW]) {
+    if (!isOnlyOneKeyDown && p.keyIsDown(key)) {
+      isOnlyOneKeyDown = true;
+      continue;
     }
-  
-    return true;
+
+    if (isOnlyOneKeyDown && p.keyIsDown(key)) {
+      return false;
+    }
   }
+
+  return true;
+}
+
+export function movement(d) {
+  if (isKeyDown(d)) {
+    player.move(0, SPEED);
+    if (player.curAnim() !== directions[d]["moveAnim"]) {
+      player.play(directions[d]["moveAnim"]);
+    }
+  }
+  if (isKeyReleased(d) && !isKeyDown(d)) {
+    player.play(directions[d]["idleAnim"]);
+  }
+}
